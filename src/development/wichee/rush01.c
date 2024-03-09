4d99f46319f34c4878e040145bd2204e4e5d831e
Wilson Wei Xiang Chee <wichee@c2r3s12.42singapore.sg>
2024-03-09 16:46:52 +0800

    initial pseudocode, no partial solved logically yet. Algo not found

---
+++ src/development/wichee/rush01.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wichee <wichee@student.42singapore.sg      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 16:07:28 by wichee            #+#    #+#             */
+/*   Updated: 2024/03/09 16:45:26 by wichee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+/* only considers the base case of 4x4 grid. Will not attempt dynamic memory allocation for variable grid sizes */
+
+/*
+int	is_validset(int input[4][4])
+{
+	check rows for values 1-4
+		return (1);
+	check rows for values 1-4
+		return (1);
+	else
+		return (0);
+}	
+*/
+
+/*
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+		write(1, str, 1);
+		str++;
+}
+*/
+
+/*
+int	*init_array(int argc, char *argv[])
+{
+	int	input[4][4];
+	int	count;
+
+	return (input);
+}
+*/
+
+/*
+int	main(int argc, char *argv[])
+{
+	int	input[4][4];
+
+	input = init_array(argc, argv)
+	if (!is_valid(input))
+		ft_putstr("Error");
+		write(1, "\n", 1);
+		return (0)
+	while (input 
+	if (c1up || c4up)
+			
+}
+*/
